54463c1c39fefd466ffa1d49059b8c34751d8ff0
nieyraud <50084366+Harthann@users.noreply.github.com>
2022-04-25 12:29:22 +0200

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -55,3 +55,14 @@ fdisk /dev/vdb <<< $(
 mkfs -v -t ext4 /dev/vdb1
 mkswap /dev/vdb1
 mkfs -v -t ext4 /dev/vdb2
+
+##########################
+##  MOUNTING PARTITION  ##
+##########################
+
+mkdir -pv $LFS
+mount -v -t ext4 /dev/vdb2 $LFS
+
+echo '/dev/vdb2  /mnt/lfs ext4   defaults      1     1' >> /etc/fstab
+
+/sbin/swapon -v /dev/vdb1
