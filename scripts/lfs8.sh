1575d4b644cd08c3696c28e2fdea62fbf4af0e7a
nieyraud <50084366+Harthann@users.noreply.github.com>
2022-04-27 21:15:12 +0200

    Create lfs8.sh

---
+++ scripts/lfs8.sh
@@ -0,0 +1,83 @@
+##  MAN PAGES ##
+
+tar -xvf man-pages-5.13.tar.xz
+cd man-pages-5.13
+
+make prefix=/usr install
+
+cd ..
+rm -rvf man-pages-5.13
+
+Iana-Etc-20220207
+Glibc-2.35
+Zlib-1.2.11
+Bzip2-1.0.8
+Xz-5.2.5
+Zstd-1.5.2
+File-5.41
+Readline-8.1.2
+M4-1.4.19
+Bc-5.2.2
+Flex-2.6.4
+Tcl-8.6.12
+Expect-5.45.4
+DejaGNU-1.6.3
+Binutils-2.38
+GMP-6.2.1
+MPFR-4.1.0
+MPC-1.2.1
+Attr-2.5.1
+Acl-2.3.1
+Libcap-2.63
+Shadow-4.11.1
+GCC-11.2.0
+Pkg-config-0.29.2
+Ncurses-6.3
+Sed-4.8
+Psmisc-23.4
+Gettext-0.21
+Bison-3.8.2
+Grep-3.7
+Bash-5.1.16
+Libtool-2.4.6
+GDBM-1.23
+Gperf-3.1
+Expat-2.4.6
+Inetutils-2.2
+Less-590
+Perl-5.34.0
+XML::Parser-2.46
+Intltool-0.51.0
+Autoconf-2.71
+Automake-1.16.5
+OpenSSL-3.0.1
+Kmod-29
+Libelf from Elfutils-0.186
+Libffi-3.4.2
+Python-3.10.2
+Ninja-1.10.2
+Meson-0.61.1
+Coreutils-9.0
+Check-0.15.2
+Diffutils-3.8
+Gawk-5.1.1
+Findutils-4.9.0
+Groff-1.22.4
+GRUB-2.06
+Gzip-1.11
+IPRoute2-5.16.0
+Kbd-2.4.0
+Libpipeline-1.5.5
+Make-4.3
+Patch-2.7.6
+Tar-1.34
+Texinfo-6.8
+Vim-8.2.4383
+MarkupSafe-2.0.1
+Jinja2-3.0.3
+Systemd-250
+D-Bus-1.12.20
+Man-DB-2.10.1
+Procps-ng-3.3.17
+Util-linux-2.37.4
+E2fsprogs-1.46.5 
